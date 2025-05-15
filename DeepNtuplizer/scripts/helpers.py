3de94c6f941adfcd362ce6e08e4873689961cee7
P. A. Jung <alexander.jung@rwth-aachen.de>
2025-05-15 14:51:42 +0200

    cleanup

--- DeepNtuplizer/scripts/helpers.py
+++ DeepNtuplizer/scripts/helpers.py
@@ -1,78 +1,94 @@
-
-
 class bcolors:
-    
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
-def resetJobOutput(parentdir,jobno):
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def resetJobOutput(parentdir, jobno):
     import os
-    rootfile=parentdir+'/output/'+parentdir+'_'+str(jobno)+'.root'
-    outs=parentdir+'/batch/con_*'+str(jobno)+'.*'
-    clusterf=parentdir+"/helper/condorcluster_"+str(jobno)+'*'
-    submitindicator=parentdir+"/helper/"+str(jobno)+".submitted"
-    os.system('rm -f '+rootfile+' '+outs+' '+clusterf+' '+submitindicator)
-    #os.system('touch '+parentdir+'/batch/con_out.'+ str(jobno) +'.out')
-    
+
+    rootfile = parentdir + "/output/" + parentdir + "_" + str(jobno) + ".root"
+    outs = parentdir + "/batch/con_*" + str(jobno) + ".*"
+    clusterf = parentdir + "/helper/condorcluster_" + str(jobno) + "*"
+    submitindicator = parentdir + "/helper/" + str(jobno) + ".submitted"
+    os.system("rm -f " + rootfile + " " + outs + " " + clusterf + " " + submitindicator)
+    # os.system('touch '+parentdir+'/batch/con_out.'+ str(jobno) +'.out')
 
 
-def submitjob(path,condorfile,jobno=-1):
+def submitjob(path, condorfile, jobno=-1):
     import subprocess
     import os
+
     print(condorfile)
-    proc = subprocess.Popen(['cd ' + path + ' && condor_submit '+ condorfile], stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen(
+        ["cd " + path + " && condor_submit " + condorfile],
+        stdout=subprocess.PIPE,
+        shell=True,
+    )
     (out, err) = proc.communicate()
-    print (out)
+    print(out)
     if err and len(err):
-        print (err)
-    elif jobno>-1:
-        os.system('touch '+path +'/helper/'+str(jobno)+".submitted")
-        #os.system('touch '+path+'/batch/con_out.'+ str(jobno) +'.out')
-    cluster=0
+        print(err)
+    elif jobno > -1:
+        os.system("touch " + path + "/helper/" + str(jobno) + ".submitted")
+        # os.system('touch '+path+'/batch/con_out.'+ str(jobno) +'.out')
+    cluster = 0
     try:
-        cluster=out.split()[-1][0:-1]
+        cluster = out.split()[-1][0:-1]
     except:
         0
     return cluster
 
-def createClusterInfo(path,job,cluster,batchsub):
+
+def createClusterInfo(path, job, cluster, batchsub):
     import os
-    addstring='0'
+
+    addstring = "0"
     if batchsub:
-        addstring=str(job)
-    clfile= open(os.path.join(path+'/helper', 'condorcluster_'+str(job)+'.'+str(cluster)+'.'+addstring), 'w')
+        addstring = str(job)
+    clfile = open(
+        os.path.join(
+            path + "/helper",
+            "condorcluster_" + str(job) + "." + str(cluster) + "." + addstring,
+        ),
+        "w",
+    )
     clfile.write(str(cluster))
     clfile.close()
 
+
 def getCondorStatus():
-    
+
     import subprocess
-    proc = subprocess.Popen(['condor_q -nobatch'], stdout=subprocess.PIPE, shell=True)
+
+    proc = subprocess.Popen(["condor_q -nobatch"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-    out=out.split('\n')
-    clustersandjobs=[]
-    statuses=[]
-    isdata=False
+    out = out.split("\n")
+    clustersandjobs = []
+    statuses = []
+    isdata = False
     for line in out:
-        if len(line)<10 : continue
-        tline=line.split()
-        if tline[0] == 'ID':
-            isdata=True
-        if tline[0] == 'ID' or tline[1] == 'jobs;' or not isdata:
+        if len(line) < 10:
+            continue
+        tline = line.split()
+        if tline[0] == "ID":
+            isdata = True
+        if tline[0] == "ID" or tline[1] == "jobs;" or not isdata:
             continue
 
         clustersandjobs.append((tline[0]))
         statuses.append(tline[5])
 
-    return clustersandjobs,statuses
+    return clustersandjobs, statuses
+
 
 def readStatuses(cluster, nJobs, condorstatus):
-    out=[]
+    out = []
     for i in range(nJobs):
-        out.append('I')
+        out.append("I")
--- DeepNtuplizer/scripts/mergeSamples.py
+++ DeepNtuplizer/scripts/mergeSamples.py
@@ -4,51 +4,52 @@ from argparse import ArgumentParser
 import os
 import subprocess
 
+
 def syscall(cmd):
-  print('Executing: %s' % cmd)
-  retval = os.system(cmd)
-  if retval != 0:
-    raise RuntimeError('Command failed!')
+    print("Executing: %s" % cmd)
+    retval = os.system(cmd)
+    if retval != 0:
+        raise RuntimeError("Command failed!")
+
 
 print("This script is still experimental and not fully completed")
 
-parser = ArgumentParser('merge samples')
-parser.add_argument('nsamples')
-parser.add_argument('outdir')
-parser.add_argument('infiles', metavar='N', nargs='+',
-                    help='sample list files')
-parser.add_argument('--batch', action='store_true')
+parser = ArgumentParser("merge samples")
+parser.add_argument("nsamples")
+parser.add_argument("outdir")
+parser.add_argument("infiles", metavar="N", nargs="+", help="sample list files")
+parser.add_argument("--batch", action="store_true")
 
 args = parser.parse_args()
 
 if not os.path.isdir(args.outdir):
-    allins=''
+    allins = ""
     for l in args.infiles:
-        allins+=' '+l
+        allins += " " + l
 
     print(str(args.nsamples))
     print(args.outdir)
     print(allins)
-    syscall('createMergeList '+str(args.nsamples)+' '+args.outdir+' '+allins)
-    
-    
-#read number of jobs
-file=open(args.outdir+'/nentries','r')
-nJobs=int(file.read())
+    syscall("createMergeList " + str(args.nsamples) + " " + args.outdir + " " + allins)
 
-listtoberun=[]
-listsucc=[]
+
+# read number of jobs
+file = open(args.outdir + "/nentries", "r")
+nJobs = int(file.read())
+
+listtoberun = []
+listsucc = []
 
 for j in range(nJobs):
-    if os.path.exists(args.outdir+'/'+str(j)+'.succ'):
+    if os.path.exists(args.outdir + "/" + str(j) + ".succ"):
         listsucc.append(j)
         continue
     listtoberun.append(j)
 
-print('successful: ',listsucc)
+print("successful: ", listsucc)
 
 if args.batch:
-	script = '''#! /bin/bash
+    script = """#! /bin/bash
 
 set -o errexit
 set -o nounset
@@ -58,63 +59,63 @@ eval `scramv1 runtime -sh`
 cd -
 
 merge {outdir}/mergeconfig $1
-'''.format(cwd=os.getcwd(), outdir=args.outdir)
-	from time import time
-	dname = '%s/merging_%d' % (os.getcwd(), int(time()))
-	os.makedirs(dname)
-	with open('%s/batch.sh' % dname, 'w') as batch:
-		batch.write(script)
-	
-	header = '''
+""".format(
+        cwd=os.getcwd(), outdir=args.outdir
+    )
+    from time import time
+
+    dname = "%s/merging_%d" % (os.getcwd(), int(time()))
+    os.makedirs(dname)
+    with open("%s/batch.sh" % dname, "w") as batch:
+        batch.write(script)
+
+    header = """
 executable = {dname}/batch.sh
 getenv = True
 use_x509userproxy = True
 +MaxRuntime = 21600
 max_transfer_output_mb = 2000
 max_retries = 4
-'''.format(dname=dname)
-	
-	template = '''
+""".format(
+        dname=dname
+    )
+
+    template = """
 arguments  = {args}
 log        = con_out.{idx}.log
 output     = con_out.{idx}.out
 error      = con_out.{idx}.err
 queue
-'''
-	jobs = [
-		template.format(args=val, idx=idx) 
-		for idx, val in enumerate(listtoberun)
-		]
-	with open('%s/condor.sub' % dname, 'w') as condor:
-		condor.write(header)
-		condor.write(''.join(jobs))
-	os.chdir(dname)
-	syscall('condor_submit condor.sub')
-	print('Once all the jobs are run please run again this command to ensure everything worked')
+"""
+    jobs = [template.format(args=val, idx=idx) for idx, val in enumerate(listtoberun)]
+    with open("%s/condor.sub" % dname, "w") as condor:
+        condor.write(header)
+        condor.write("".join(jobs))
+    os.chdir(dname)
+    syscall("condor_submit condor.sub")
+    print(
+        "Once all the jobs are run please run again this command to ensure everything worked"
+    )
 else:
-	import multiprocessing as mp
+    import multiprocessing as mp
 
-	def worker(j):
-		print('starting '+str(j))
-		os.system('merge '+args.outdir+'/mergeconfig '+str(j))
+    def worker(j):
+        print("starting " + str(j))
+        os.system("merge " + args.outdir + "/mergeconfig " + str(j))
 
-	pool = mp.Pool(processes=mp.cpu_count(),) 
-	pool.map(worker, listtoberun)
+    pool = mp.Pool(
+        processes=mp.cpu_count(),
+    )
+    pool.map(worker, listtoberun)
 
 
 for j in range(nJobs):
-    if os.path.exists(args.outdir+'/'+str(j)+'.succ'):
+    if os.path.exists(args.outdir + "/" + str(j) + ".succ"):
         listsucc.append(j)
-    
+
 if len(listsucc) == nJobs:
-    print('merge successful, creating file list')
-    file=open(args.outdir+'/samples.txt','w')
+    print("merge successful, creating file list")
+    file = open(args.outdir + "/samples.txt", "w")
     for filenumber in listsucc:
-        file.write('ntuple_merged_'+str(filenumber)+'.root\n')
+        file.write("ntuple_merged_" + str(filenumber) + ".root\n")
     file.close()
-
-
-
-
-
-
